bb1b7a34bfc04322be6bd655e0b321ee27a20991
A_SCHMITZ <schmitzandrew03@gmail.com>
2021-01-30 22:36:27 -0800

    prints the question :)

--- src/cogs/questions.py
+++ src/cogs/questions.py
@@ -8,23 +8,22 @@ import re
 
 def pick_question():
     print("csv reading start")
-    questions =[]
+    splitquestion = []
     with open('Test Sheet.csv', newline='') as csvfile:
-        questions = csvfile.readlines()
-    print(questions[0])
-    del questions[0]
-    question = []
-    strquestion = random.choice(questions)
-    for part in reader(strquestion):
-        question.append(part)
-    return question
+        questions = list(reader(csvfile))
+        print(questions[0])
+        del questions[0]
+        question = random.choice(questions)
+        for part in reader(question):
+            splitquestion.append(part)
+    return splitquestion
+    #split the string into questions
 
 async def send_question(bot, channel, question):
-    for i in question:
-        print(i)
-    await channel.send("This is a " + question[0] + " question.")
-    await channel.send("Question: " + question[1])
-    await channel.send("Description: " + question[2])
+    await channel.send("This is a " + str(question[0]) + " question.")
+    await channel.send("Question: " + str(question[1]))
+    await channel.send("Description: " + str(question[2]))
+    await channel.send("Sample Answer: " + str(question[3]))
     #await channel.send()
 
 
